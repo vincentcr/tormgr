70d600f7513719f84b327101dd5fc068bf7dae6f
Vincent Côté-Roy <vincentcr@gmail.com>
2015-12-27 14:44:24 -0500

    simplify db access methods
    * use sqlx NamedExec to allow named parameters;
    * add dbExecOnRecord method to encapsulate data-modification queries (update/insert/delete)
    * add CacheHinter interface to simplify dbFind* invocation
    * add update/delete methods in Torrent and Folder
    * Torrent: replace folderID ref with folder name, this saves us from joining just to get the folder name
    * Torrent: add status field to manage downloads

--- api/db/sql/schema.sql
+++ api/db/sql/schema.sql
@@ -1,16 +1,25 @@
 
 --CREATE DOMAIN uid NOT NULL CHECK(VALUE ~ '^[a-fA-F0-9]{32}$');
 
+CREATE OR REPLACE FUNCTION update_modified_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.date_modified = now();
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+
 CREATE TABLE users(
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   email TEXT NOT NULL UNIQUE CHECK(email ~ '^[a-zA-Z0-9_%+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9][a-zA-Z0-9]+$'),
-  password VARCHAR(128) NOT NULL
+  password TEXT NOT NULL
 );
 
 CREATE UNIQUE INDEX idx_users_email ON users(lower(email));
 
 CREATE TABLE access_tokens(
-  secret VARCHAR(256) PRIMARY KEY,
+  secret TEXT PRIMARY KEY,
   user_id uuid REFERENCES users(id) NOT NULL,
   expires TIMESTAMP,
   access INT NOT NULL
@@ -22,22 +31,27 @@ CREATE TABLE folders(
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   owner_id uuid REFERENCES users(id) NOT NULL,
   date_created TIMESTAMP NOT NULL DEFAULT timeofday()::TIMESTAMP,
-  name VARCHAR(1024) NOT NULL CHECK (name != '')
+  date_modified TIMESTAMP NOT NULL DEFAULT timeofday()::TIMESTAMP,
+  name CITEXT NOT NULL CHECK (name != ''),
+  UNIQUE(owner_id, name)
 );
 CREATE INDEX idx_folders_owner_id ON folders(owner_id);
 CREATE INDEX idx_folders_id_owner_id ON folders(id, owner_id);
-CREATE UNIQUE INDEX idx_folders_owner_id_name ON folders(owner_id, lower(name));
-
+CREATE TRIGGER update_modtime BEFORE UPDATE ON folders FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
 
 CREATE TABLE torrents(
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  folder VARCHAR(1024) REFERENCES folders(name) ON DELETE CASCADE NOT NULL,
+  folder CITEXT NOT NULL,
   owner_id uuid REFERENCES users(id) NOT NULL,
   date_created TIMESTAMP NOT NULL DEFAULT timeofday()::TIMESTAMP,
+  date_modified TIMESTAMP NOT NULL DEFAULT timeofday()::TIMESTAMP,
   info_hash TEXT NOT NULL CHECK (info_hash ~ '^[A-F0-9]{40}$'),
   source_url TEXT,
-  data BYTEA
+  data BYTEA,
+  status TEXT NOT NULL DEFAULT 'new' CHECK (status IN ('new', 'downloading', 'downloaded', 'failed')),
+  FOREIGN KEY (folder, owner_id) REFERENCES folders(name, owner_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  UNIQUE(id, info_hash)
 );
 CREATE INDEX idx_torrents_folder ON torrents(folder);
 CREATE INDEX idx_torrents_id_owner_id ON torrents(id, owner_id);
-CREATE UNIQUE INDEX idx_torrents_id_info_hash ON torrents(id, info_hash);
+CREATE TRIGGER update_modtime BEFORE UPDATE ON torrents FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
--- api/folders.go
+++ api/folders.go
@@ -1,57 +1,35 @@
 package main
 
-import "fmt"
-
 type Folder struct {
-	ID      RecordID  `json:"id"`
-	Name    string    `json:"title"`
-	Items   []Torrent `json:"items"`
-	ownerID RecordID
+	ID      RecordID `json:"id"`
+	Name    string   `json:"name"`
+	OwnerID RecordID `json:"omit" db:"owner_id" `
 }
 
-func FolderCreate(user User, token string, folder *Folder) error {
-	if folder.ID == "" {
-		folder.ID = newID()
-	}
-
-	if folder.Items == nil {
-		folder.Items = []Torrent{}
-	}
-
-	folder.ownerID = user.ID
-
-	tx, err := services.db.Begin()
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
-
-	_, err = tx.Exec("INSERT INTO folders(id,owner_id,name) VALUES($1,$2,$)",
-		folder.ID, folder.ownerID, folder.Name)
-	if err != nil {
-		if isUniqueError(err) {
-			return ErrUniqueViolation
-		} else {
-			return fmt.Errorf("unable to create folder %#v: %v", folder, err)
-		}
-	}
-
-	// err = addTorrents(user, folder.ID, folder.Items, tx)
-	// if err != nil {
-	// 	return err
-	// }
-
-	// invalidateCache(folderCacheHint{user, folder.ID})
-
-	return tx.Commit()
+func (f *Folder) cacheHint() cacheHint {
+	return cacheHint{userID: f.OwnerID, table: "folders", recordID: f.ID}
 }
 
 func FolderGetAll(user User) (Cacheable, error) {
-	cacheHint := cacheHint{userID: user.ID, table: "folders"}
-	return dbFind([]Folder{}, cacheHint, "SELECT id,name from folders where owner_id=?", user.ID)
+	return dbFind(&Folder{OwnerID: user.ID}, "SELECT id,name from folders where owner_id=?", user.ID)
 }
 
 func FolderGet(user User, id RecordID) (Cacheable, error) {
-	cacheHint := cacheHint{userID: user.ID, table: "folders", recordID: id}
-	return dbFindOne(Folder{}, cacheHint, "SELECT id,name from folders where id=? AND owner_id=?", id, user.ID)
+	return dbFindOne(&Folder{OwnerID: user.ID, ID: id}, "SELECT id,name from folders where owner_id=?", user.ID)
+
+}
+
+func FolderCreate(user User, name string) (Folder, error) {
+	folder := Folder{ID: newID(), OwnerID: user.ID, Name: name}
+
+	err := dbExecOnRecord("insert", "INSERT INTO folders(id, owner_id,name) VALUES(:id, :owner_id, :name)", &folder)
+	return folder, err
+}
+
+func FolderRename(folder Folder) error {
+	return dbExecOnRecord("update", "UPDATE folders SET name = :name WHERE id = :id AND owner_id = :owner_id", &folder)
+}
+
+func FolderDelete(folder Folder) error {
+	return dbExecOnRecord("delete", "DELETE FROM folders where id = :id AND owner_id = :owner_id", &folder)
 }
